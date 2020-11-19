bec24685fefb25410b5c013c0fabdc640db96f52
Mike Sperber <sperber@deinprogramm.de>
2020-11-19 14:26:04 +0100

    Update

--- rev.rkt
+++ rev.rkt
@@ -2,12 +2,12 @@
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "vanilla-reader.rkt" "deinprogramm" "sdp")((modname rev) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
 ; Liste umdrehen
-(: rev ((list-of %a) -> (list-of %a)))
+#;(: rev ((list-of %a) -> (list-of %a)))
 
-(check-expect (rev (list 1 2 3))
+#;(check-expect (rev (list 1 2 3))
               (list 3 2 1))
 
-(define rev
+#;(define rev
   (lambda (list)
     (cond
       ((empty? list) empty)
