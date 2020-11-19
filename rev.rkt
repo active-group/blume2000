9694cb837225160bad685b523cd58057d729dd10
Mike Sperber <sperber@deinprogramm.de>
2020-11-19 14:20:37 +0100

    Update

--- rev.rkt
+++ rev.rkt
@@ -1,3 +1,9 @@
 ;; Die ersten drei Zeilen dieser Datei wurden von DrRacket eingefügt. Sie enthalten Metadaten
 ;; über die Sprachebene dieser Datei in einer Form, die DrRacket verarbeiten kann.
 #reader(lib "vanilla-reader.rkt" "deinprogramm" "sdp")((modname rev) (read-case-sensitive #f) (teachpacks ()) (deinprogramm-settings #(#f write repeating-decimal #f #t none explicit #f ())))
+; Liste umdrehen
+(: rev ((list-of %a) -> (list-of %a)))
+
+(check-expect (rev (list 1 2 3))
+              (list 3 2 1))
+
