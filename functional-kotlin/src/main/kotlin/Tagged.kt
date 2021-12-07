06d44f99cd35a9da462e2e72bf9fbb9adf01317a
Mike Sperber <sperber@deinprogramm.de>
2021-12-07 16:08:44 +0100

    Update.

--- functional-kotlin/src/main/kotlin/Tagged.kt
+++ functional-kotlin/src/main/kotlin/Tagged.kt
@@ -1,2 +1,12 @@
-class Tagged {
-}
+data class Foo(val a: Int, val b: Int)
+
+// T: Phantom-Typ
+data class Tagged<T, out A>(val value: A)
+
+class NonZero {}
+fun nonZero(foo: Tagged<Nothing, Foo>): Tagged<NonZero, Foo> =
+    Tagged(foo.value.a != 0 && foo.value.b != 0)
+
+class Greater {}
+fun greater(foo: Foo): Boolean = foo.a > foo.b
+
