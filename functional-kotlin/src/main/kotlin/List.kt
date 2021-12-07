1f1859b3558c67b0529a354fde7a1912e5b6cce2
Mike Sperber <sperber@deinprogramm.de>
2021-12-07 13:58:38 +0100

    Update.

--- functional-kotlin/src/main/kotlin/List.kt
+++ functional-kotlin/src/main/kotlin/List.kt
@@ -5,7 +5,7 @@
 // Eine Liste ist eins der folgenden:
 // - die leere Liste
 // - eine Cons-Liste besteht aus erstem Element und Rest-Liste
-//                                                       ^^^^^
+//                                                       ^^^^^ Selbstreferenz
 // gemischte Daten
 // Kotlin: ein Interface obendrüber, Unterklassen
 sealed interface List<out A>
@@ -25,9 +25,24 @@ data class Cons<out A>(val first: A, val rest: List<A>) : List<A>
 
 // Summe aller Listenelemente
 // wir brauchen: List<Nothing> < List<Int> für Empty
-fun listSum(list: List<Int>): Int =
+tailrec fun listSum(list: List<Int>): Int =
     when (list) {
         is Empty -> 0
         is Cons ->
+            // list.first + []: Kontext des Aufrufs von listSum
             list.first + listSum(list.rest)
     }
+
+tailrec fun listSum1(list: List<Int>, acc: Int = 0): Int =
+    when (list) {
+        is Empty -> acc
+        is Cons ->
+            // kein Kontext, endrekursiv
+            listSum1(list.rest, acc + list.first)
+    }
+
+val list1 = Cons(1, Empty)
+val list2 = Cons(1, Cons(2, Empty))
+// 3elementige Liste: 3 5 7
+val list3 = Cons(3, Cons(5, Cons(7, Empty)))
+val list4 = Cons(1, list3)
