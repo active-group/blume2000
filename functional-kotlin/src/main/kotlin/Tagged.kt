bc64c5acea9c079bd9a7f6b84050858fb6f417ff
Mike Sperber <sperber@deinprogramm.de>
2021-12-07 16:10:32 +0100

    Update.

--- functional-kotlin/src/main/kotlin/Tagged.kt
+++ functional-kotlin/src/main/kotlin/Tagged.kt
@@ -4,9 +4,16 @@ data class Foo(val a: Int, val b: Int)
 data class Tagged<T, out A>(val value: A)
 
 class NonZero {}
-fun nonZero(foo: Tagged<Nothing, Foo>): Tagged<NonZero, Foo> =
-    Tagged(foo.value.a != 0 && foo.value.b != 0)
+fun nonZero(foo: Tagged<Nothing, Foo>): Option<Tagged<NonZero, Foo>> =
+    if (foo.value.a != 0 && foo.value.b != 0)
+        Some(Tagged(foo.value))
+    else
+        None
 
 class Greater {}
-fun greater(foo: Foo): Boolean = foo.a > foo.b
+fun greater(foo: Tagged<NonZero, Foo>): Option<Tagged<Greater, Foo>> =
+    if (foo.value.a > foo.value.b)
+        Some(Tagged(foo.value))
+    else
+        None
 
