b054c1e17b2d01b01de4101bbd03523b9134db9c
Mike Sperber <sperber@deinprogramm.de>
2021-12-07 16:30:57 +0100

    Update.

--- functional-kotlin/src/main/kotlin/DB.kt
+++ functional-kotlin/src/main/kotlin/DB.kt
@@ -1,2 +1,29 @@
-class DB {
+// Idee:
+// Domäne liefert eine Beschreibung des Ablaufs
+// Bsp: Validierung, Verfügbarkeit prüfen etc.
+
+// "Ablauf"
+// Datenbank-Programm, besteht aus get und put
+// put("Mike", 50)
+// x <- get("Mike")
+// put("Mike", x + 1)
+// y <- get("Mike")
+// return x + y
+
+// sealed interface DBCommand
+// data class Get(val key: String): DBCommand
+// data class Put(val key: String, val value: String): DBCommand
+
+// DBProgram = List<DBCommand>
+
+// Wie etwas einen Namen geben?
+// FP: Lambda!
+// NodeJS: Callback, FP: Continuation
+
+sealed interface DB
+data class Get(val key: String, val cont: (Int) -> DB)
+data class Put(val key: String, val value: Int, val cont: (Unit) -> DB)
+
+val p1 = Put("Mike", 50) { () ->
+    TODO()
 }
